8d2d6a82a58b863d80685c0519e52aa77a79c11d
LEXIN Website Deployment <sales@lexinmetal.com>
2026-07-20 01:29:49 +0800

    Restrict selected products to staff

--- assets/data/content-visibility.js
+++ assets/data/content-visibility.js
@@ -1,4 +1,4 @@
 window.lexinContentVisibility = {
   inspirationStaffOnly: ["3707", "3708", "3709", "3710"],
-  productStaffOnly: [],
+  productStaffOnly: ["LX3008", "LX3009", "XH7830", "LX7539", "LX5153"],
 };
